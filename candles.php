088c2be5955418538fa36a047b1e19550294d6dc
Mike Haiduk <x29a100@gmail.com>
2024-10-24 19:25:01 +0300

    update open dialog animation

--- candles.php
+++ candles.php
@@ -4,7 +4,7 @@ include_once $_SERVER["DOCUMENT_ROOT"] . "/mfm-analytics/utils.php";
 $key = get_required(key);
 $period_name = get_required(period_name);
 
-$response[candles] = getCandles($key, $period_name);
+$response[candles] = getCandles($key, $period_name, 30);
 $response[value] = getCandleLastValue($key);
 $response[change24] = getCandleChange24($key);
 
