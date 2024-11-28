c329a527a5f1573fe196ee78bf85764f89abe3d5
Mike Haiduk <x29a100@gmail.com>
2024-11-28 16:04:15 +0100

    small bugfixes

--- init.php
+++ init.php
@@ -14,7 +14,7 @@ query("CREATE TABLE IF NOT EXISTS `candles` (
   `close` float NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;");
 
-query("DROP TABLE IF EXISTS `events`;");
+//query("DROP TABLE IF EXISTS `events`;");
 query("CREATE TABLE IF NOT EXISTS `events` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parent` varchar(64) COLLATE utf8_bin NULL,
--- review/controller.js
+++ review/controller.js
@@ -3,12 +3,12 @@ function openReview(success) {
     showBottomSheet('/mfm-analytics/review/index.html', success, function ($scope) {
         $scope.like = function like() {
             trackCall(arguments)
-            showSuccess("Thank you for your feedback", $scope.back)
+            showSuccess(str.trank_you_for_your_feedback, $scope.back)
         }
 
         $scope.dislike = function dislike(message) {
             trackCall(arguments)
-            showSuccess("Thank you for your feedback", $scope.back)
+            showSuccess(str.trank_you_for_your_feedback, $scope.back)
         }
     })
 }
